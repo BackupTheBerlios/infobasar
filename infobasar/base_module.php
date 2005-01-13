651f13d1402c1fccf8b86634c1bd94c0aa8a8e18
hamatoma <hamatoma>
2005-01-13 03:48:41 +0000

    Version

--- infobasar/base_module.php
+++ infobasar/base_module.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: base_module.php,v 1.7 2005/01/13 03:34:37 hamatoma Exp $
+// $Id: base_module.php,v 1.8 2005/01/13 03:48:41 hamatoma Exp $
 /*
 Diese Datei ist Teil von InfoBasar.
 Copyright 2004 hamatoma@gmx.de München
@@ -10,7 +10,7 @@ InfoBasar sollte nützlich sein, es gibt aber absolut keine Garantie
 der Funktionalität.
 */
 $start_time = microtime ();
-define ('PHP_ModuleVersion', '0.7.1 (2005.01.11)');
+define ('PHP_ModuleVersion', '0.7.2 (2005.01.13)');
 set_magic_quotes_runtime(0);
 error_reporting(E_ALL);
 
