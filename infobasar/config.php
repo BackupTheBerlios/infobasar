6206bd303a3e8c79b56263aed763d2f8afd9a785
hamatoma <hamatoma>
2005-01-14 03:09:16 +0000

    Trace-Schalter jetzt hier

--- infobasar/config.php
+++ infobasar/config.php
@@ -5,4 +5,10 @@ $db_user = 'forum';
 $db_passw = 'forumforum';
 $db_name = 'infobasar';
 $db_prefix = 'infobasar_';
+
+$_GET ['TraceFlags'] = 0; // -1: aus 0: TC_X, 1: special 2: all
+$_GET ['TraceInFile'] = false;
+$_GET ['RemoveTraceFile'] = true;
+#$_GET ['TraceInFile'] = true;
+#$this->fTraceInFile = true;
 ?>
